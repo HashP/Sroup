8715d4c022179611e6464579381c3c162ae6f8e7
CJ <cool2shop@naver.com>
2015-07-24 21:42:16 +0900

    이미지 파일 하나 회원관리 메인페이지 작성

--- sroup/src/main/java/com/cj/sroup/controller/ManagementController.java
+++ sroup/src/main/java/com/cj/sroup/controller/ManagementController.java
@@ -1,5 +1,13 @@
 package com.cj.sroup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ManagementController {
 
+	@RequestMapping("/m_main.do")
+	public String main(){
+		return "m_main.jsp";
+	}
 }
